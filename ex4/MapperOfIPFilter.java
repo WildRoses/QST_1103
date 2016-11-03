2b81fee6b067d142a65bec3aaef998ee1880f846
WildRoses <1538257077@qq.com>
2016-11-03 04:17:40 -0500

    Add files via upload

--- ex4/MapperOfIPFilter.java
+++ ex4/MapperOfIPFilter.java
@@ -1,12 +1,29 @@
 package No4;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MapperOfIPFilter {
-	public static void main(String[] args){
+	public static void main(String[] args) throws ParseException {
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
+		SimpleDateFormat regularFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", Locale.US);
+		Date beginDate = regularFormat.parse(args[0]);// 输入设置开始时间
+		Date endDate = regularFormat.parse(args[1]);//输入设置结束时间
+		String strIp = null;
+		Date lineDate = null;
+		String strTime = null;
+		while (scanner.hasNext()) {
 			String line = scanner.nextLine();
+			strIp = line.split(" ")[0];// 获取ip
+			strTime = line.split(" ")[3].substring(1, line.split(" ")[3].length());// 截取时间
+			lineDate = regularFormat.parse(strTime);
+			// 判断时间的范围，筛选出需要的时间段
+			if (lineDate.before(endDate) && lineDate.after(beginDate)) {
+				System.out.println(strIp + "\t" + strTime);
+			}
 		}
 	}
 }
--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,18 @@
 package No4;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class ReducerOfIPFilter {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		String InIp=null;//储存ip
+		HashSet<String> set=new HashSet<String>(); //记录不重复的ip
 		while (scanner.hasNext()){
 			String line = scanner.nextLine();
+			InIp=line.split("\t")[0];
+			set.add(InIp);
 		}
+		System.out.println("该时间段访问的ip数"+set.size());//ip的数量，即是set的大小
 	}
 }
